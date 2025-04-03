f081c791ab21c9ff418c1055657ebdd9706fb994
IanMo-fr <2.ianmoise@gmail.com>
2025-04-04 00:39:38 +0200

    renommage bonne pratique ..

--- src/test/java/KarateRunnerTest.java
+++ src/test/java/KarateRunnerTest.java
@@ -1,6 +1,6 @@
 import com.intuit.karate.junit5.Karate;
 
-public class KarateRunner {
+public class KarateRunnerTest {
     @Karate.Test
     Karate testAll() {
         return Karate.run("classpath:test-ti-endpoints.feature").relativeTo(getClass());
