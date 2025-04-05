169316f2e04fca4def8053e0e1b435fb1d4567e8
IanMo-fr <2.ianmoise@gmail.com>
2025-04-05 19:00:39 +0200

    ajout sénario

--- src/test/java/KarateRunnerTest.java
+++ src/test/java/KarateRunnerTest.java
@@ -3,6 +3,6 @@ import com.intuit.karate.junit5.Karate;
 public class KarateRunnerTest {
     @Karate.Test
     Karate testAll() {
-        return Karate.run("classpath:test-ti-endpoints.feature").relativeTo(getClass());
+        return Karate.run("classpath:auth-keycloak.feature", "classpath:test-ti-endpoints.feature").relativeTo(getClass());
     }
 }
